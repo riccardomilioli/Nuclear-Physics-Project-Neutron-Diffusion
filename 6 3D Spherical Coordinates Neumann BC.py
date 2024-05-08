63eebff8bc89a38adcf594fc930aeaca81abff4a
Riccardo Milioli <167012797+riccardomilioli@users.noreply.github.com>
2024-05-08 10:14:15 +0200

    Update 6 3D Spherical Coordinates Neumann BC.py

--- 6 3D Spherical Coordinates Neumann BC.py
+++ 6 3D Spherical Coordinates Neumann BC.py
@@ -39,7 +39,7 @@ def eqn(x):
     return -1 + R0 * np.sqrt((eta_val + x)/mu_val) * cot(R0 * np.sqrt((eta_val + x)/mu_val)) + 3/2 * R0/lambda_t
 
 guess2 = - 4.5797e6 # Can check by hand that for this guess the value of "eqn" is close to 0
-alpha = fsolve(eqn, guess2)[0] # Different from the one in paper 
+alpha = fsolve(eqn, guess2)[0] # Different from the one on paper 
 
 # Constants needed to define the neutron density:
 k = np.sqrt((eta_val + alpha) / mu_val)
