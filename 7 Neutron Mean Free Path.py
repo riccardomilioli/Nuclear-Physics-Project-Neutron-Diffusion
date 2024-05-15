f601787efd6338dfbecad1ad6335935cccf03d3b
Riccardo Milioli <167012797+riccardomilioli@users.noreply.github.com>
2024-05-15 15:34:31 +0200

    Update 7 Neutron Mean Free Path.py

--- 7 Neutron Mean Free Path.py
+++ 7 Neutron Mean Free Path.py
@@ -6,27 +6,27 @@ import matplotlib.pyplot as plt
 # We want to compute the mean free path of a single neutron crossing a slab of material.
 
 # Parameters of the problem:
-rho = 18.71  # [g/cm^3]. Density
+rho = 18.71 * 10**6  # [g/m^3]. Density
 sigma = 1.235e-28  # [m^2]. Neutron cross-sectional area
-n = 10**6 * (rho * 6.022e23 / 235.04)  # [1/m^3]. Number density of nuclei in the slab
+n = (rho * 6.022e23 / 235.04)  # [1/m^3]. Number density of nuclei in the slab
 
-# Mean free path calculation:
-xm_integrand = lambda x: x * sigma * n * np.exp(- sigma * n * x)
-xm, _ = quad(xm_integrand, 0, math.inf) # We're setting L = math.inf (should be understood as L very large)
+# Calculating the mean free path
+xmfp_integrand = lambda x: x * sigma * n * np.exp(- sigma * n * x)
+xmfp, _ = quad(xmfp_integrand, 0, math.inf) # We're setting L = math.inf (should be understood as L very large)
 
-print(f"The mean free path (for large L) is: {xm} m")
+print(f"The mean free path (for large L) is: {xmfp} m")
 
-# Plotting the solutions:
+# Plotting the probabilities:
 x_vals = np.linspace(0, 2, 100)
 prob_direct_escape = np.exp(- sigma * n * x_vals)
 prob_react = 1 - prob_direct_escape
 
 plt.figure(figsize=(10, 6))
 plt.subplot(311)
-plt.plot(x_vals, x_vals * prob_direct_escape)
+plt.plot(x_vals, x_vals * prob_direct_escape * sigma * n)
 plt.title('Mean free path distribution')
 plt.xlabel('x')
-plt.ylabel('Distribution')
+plt.ylabel(r'$\sigma ~ n ~ x ~ e^{-\sigma n x}$') 
 
 plt.subplot(312)
 plt.plot(x_vals, prob_direct_escape)
